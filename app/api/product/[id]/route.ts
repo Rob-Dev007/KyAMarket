20e9477347db3ca4de34e2cc7c84c0d8b75087ca
Rob-Dev007 <robdev0695@gmail.com>
2025-04-25 09:38:10 -0500

    fix: depuración de errores en eliminar producto

--- app/api/product/[id]/route.ts
+++ app/api/product/[id]/route.ts
@@ -1,20 +1,26 @@
 import { getCurrentUser } from "@/actions/getCurrentUser";
 import { NextResponse } from "next/server";
 import prisma from "@/libs/prismadb";
+import { NextRequest } from "next/server";
 
-export async function DELETE(req: Request, { params }: { params: { id: string }}){
-    const currentUser = await getCurrentUser();
+export async function DELETE(req: NextRequest) {
+  const currentUser = await getCurrentUser();
 
-    if(!currentUser) return NextResponse.error();
+  if (!currentUser) return new NextResponse(null, { status: 401 });
 
-    if(currentUser.role !== 'ADMIN'){
-        return NextResponse.error();
-    }
+  if (currentUser.role !== 'ADMIN') {
+    return new NextResponse(null, { status: 403 });
+  }
 
-    const product = await prisma?.product.delete({
-        where: { id: params.id }
-    })
+  // Obtener ID desde la URL
+  const url = new URL(req.url);
+  const id = url.pathname.split("/").pop(); // obtiene el último segmento del path
 
+  if (!id) return new NextResponse(null, { status: 400 });
 
-    return NextResponse.json(product);
+  const product = await prisma.product.delete({
+    where: { id },
+  });
+
+  return NextResponse.json(product);
 }

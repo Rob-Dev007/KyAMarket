b6bc3d50961749efb3242daf40c07ce6bd59def7
Rob-Dev007 <robdev0695@gmail.com>
2026-02-03 09:43:40 -0500

    config(next): actualiza remotePatterns para imágenes externas

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,16 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images:{
-    domains: ['lh3.googleusercontent.com'],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
       },
     ],
   },

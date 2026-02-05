1186d6de5eb0e1e9dde97ee16b35075bef12ef46
Rob-Dev007 <robdev0695@gmail.com>
2026-02-05 00:01:53 -0500

    refactor(null-data): remueve React.FC y mejora espaciado en título

--- app/components/NullData.tsx
+++ app/components/NullData.tsx
@@ -2,10 +2,10 @@ interface NullDataProps{
     title: string
 }
 
-const NullData: React.FC<NullDataProps> = ({ title })=>{
+const NullData = ({ title }: NullDataProps)=>{
     return(
         <div className="w-full h-[50vh] text-xl md:text-2xl flex justify-center items-center">
-            <p className="font-medium">{ title }</p>
+            <p className="font-medium mx-2">{ title }</p>
         </div>
     )
 }
